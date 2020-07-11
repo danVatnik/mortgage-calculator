1edb89346f0f843d1ef0dc15c1ad3596a6637834
Dan Vatnik <dan.vatnik@gmail.com>
2020-07-11 18:00:10 -0400

    Adding total rent spent

--- mortage-calculator/rent.r
+++ mortage-calculator/rent.r
@@ -4,18 +4,33 @@ rent = setRefClass("rent",
   contains = "positiveNumberValidator",
   fields = list(
     monthlyCost = 'numeric',
-    yearlyIncrease = 'numeric'
+    yearlyIncrease = 'numeric',
+    numberYears = 'numeric',
+    totalSpent = 'numeric'
   ),
   methods = list(
-    initialize = function(mC, yI){
-      validateInputs(mC, yI)
+    initialize = function(mC, yI, nY){
+      validateInputs(mC, yI, nY)
       
       monthlyCost <<- mC
       yearlyIncrease <<- yI
+      numberYears <<- nY
+      
+      calculateTotalRentSpent(monthlyCost, yearlyIncrease, numberYears)
     },
-    validateInputs = function(mC, yI){
+    calculateTotalRentSpent = function(mC, yI, nY){
+      total = 0
+      currentMonthlyCost = mC
+      for (year in 1:nY) {
+         total = total + currentMonthlyCost * 12
+         currentMonthlyCost = currentMonthlyCost * (1 + yI / 100)
+      }
+      totalSpent <<- total
+    },
+    validateInputs = function(mC, yI, nY){
       validateNumber(mC)
       validateNumber(yI)
+      validateNumber(nY)
     }
   )
 )
